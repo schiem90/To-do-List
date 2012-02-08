153f8f1fb72cfdb1a4de2c2cb103995f6b134735
James Moore <trey.mo@gmail.com>
2012-02-08 12:25:03 -0500

    Created an Android project in Eclipse.

---
+++ src/gatech/todoapp/ToDoListActivity.java
@@ -0,0 +1,13 @@
+package gatech.todoapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ToDoListActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
