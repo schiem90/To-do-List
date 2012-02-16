a288daba0533f4dda26caeb3a074c7af679d8644
npicon <nickpicon@gatech.edu>
2012-02-16 15:41:38 -0500

    Added Comment

--- src/gatech/todoapp/ToDoListActivity.java
+++ src/gatech/todoapp/ToDoListActivity.java
@@ -9,5 +9,6 @@ public class ToDoListActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        //trey is gay
     }
 }
