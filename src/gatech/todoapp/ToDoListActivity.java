a24301617666986115e8d28d0a310f54693853b5
Kevin Chu <kchu9@gatech.edu>
2012-02-25 01:39:21 -0500

    Working on the registering stuff.

--- src/gatech/todoapp/ToDoListActivity.java
+++ src/gatech/todoapp/ToDoListActivity.java
@@ -11,6 +11,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class ToDoListActivity extends Activity {
 	
@@ -23,7 +24,7 @@ public class ToDoListActivity extends Activity {
         setContentView(R.layout.main);
         
         
-        //trying to make a button work.
+        //Register  Button Listener.
         Button createAccount=(Button) findViewById(R.id.register);
         createAccount.setOnClickListener(new View.OnClickListener(){
         	public void onClick(View v){
@@ -34,13 +35,24 @@ public class ToDoListActivity extends Activity {
         	}
             });
         Button Login=(Button) findViewById(R.id.login);
+        final EditText userTextBox  = (EditText)findViewById(R.id.usernameTextBox);
+        final EditText passTextBox  = (EditText)findViewById(R.id.passwordTextBox);
+        //Login Button Listener
         Login.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
 				// TODO attempt to login (used Trey's test code)
-				//TREY'S TEST CODE FOR TESTING DB STUFF
+				
 		    	db= new DatabaseUtil(ToDoListActivity.this);
-		    	//user the user you registered above to test
-		    	User currentUser = db.loginUser("Trey", "12"); // returns null b/c trey not in db
+		    	
+		    	String username = userTextBox.getText().toString();
+		    	String password= passTextBox.getText().toString();
+		    	System.out.println(username+password);
+		    	//User currentUser = db.loginUser("Trey", "12"); // returns null b/c trey not in db
+		    	//preloading trey into database to check if working.
+		    	User newUser = new User("Trey Moore", "Trey", "trey@email.com", "123");
+		    	db.registerUser(newUser);
+		    	User currentUser =db.loginUser(username, password);
+		    	
 		    	//Dialog box to see what info DB is getting
 		    	AlertDialog alertDialog = new AlertDialog.Builder(ToDoListActivity.this).create();
 		    	alertDialog.setTitle("Logged In As");
